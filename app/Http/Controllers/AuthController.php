dabc107430a709f60333228a3f431a954a9aebe9
rifky <rifkyqw@gmail.com>
2019-03-10 20:33:28 +0700

    Auth Handle Rest API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Transformer\UserTransformer;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(Request $request, User $user)
     {
-        return "register";
+        $this->validate($request,[
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:6'
+        ]);
+        
+    $cek =  $user->create([
+            'name'      => $request->name,
+            'password'  => bcrypt($request->password),
+            'email'     => $request->email,
+            'api_token' => bcrypt($request->email)
+        ]);
+    $response =  fractal()
+            ->item($cek)
+            ->transformWith(new UserTransformer)
+            ->toArray();
+    
+            return response()->json($response, 201);
+    
+    }
+
+    public function login(Request $request, User $user)
+    {
+        $credentials = $request->only('email','password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Email and Password no Match'],401);
+        }
+
+        $user = $user->find(Auth::user()->id);
+
+        return fractal()
+                ->item($user)
+                ->transformWith(new UserTransformer)
+                ->toArray();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,5 @@ use Illuminate\Http\Request;
 |
 */
 Route::post('auth/register','AuthController@register');
+Route::post('auth/login','AuthController@login');
 Route::get('users','UserController@users');
